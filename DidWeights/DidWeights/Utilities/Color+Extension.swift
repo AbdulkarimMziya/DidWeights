8242a90f44e5c47231f2d140af7e9fa081537026
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2026-08-16 10:41:09 -0700

    Add light/dark colour theme(#16)
    Co-authored-by: Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>

---
+++ DidWeights/DidWeights/Utilities/Color+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  Color+Extension.swift
+//  DidWeights
+//
+//  Created by Abdulkarim Mziya on 2026-08-14.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let appBg = Color("AppBackground")
+    static let cardBg = Color("CardRowBG")
+    static let primaryBtn = Color("PrimaryBtnBG")
+    static let primaryBtnTxt = Color("PrimaryBtnText")
+    static let pillBg = Color("PillBackground")
+    static let deleteBtnBg = Color("DeleteBtnBG")
+    static let deleteBtnTxt = Color("DeleteBtnText")
+    static let completedRowBg = Color("CompletedRowBG")
+    static let checkmarkBoxBg = Color("CheckmarkBoxBG")
+    static let checkmarkIcon = Color("CheckMarkIcon")
+    static let inputfieldBorder = Color("InputFieldBorder")
+    static let primaryHeadingTxt = Color("PrimaryHeadingText")
+    static let secondaryTxt = Color("SecondaryText")
+    
+}
