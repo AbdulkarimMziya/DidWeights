866e2280390e19365980eca5e0de4945c3304cb9
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-09-04 16:57:22 -0700

    feat: add ExerciseSet.isCompletable as single source of truth for set completability (M7)

--- DidWeights/DidWeights/Models/Extenstions/ExerciseSet+Derived.swift
+++ DidWeights/DidWeights/Models/Extenstions/ExerciseSet+Derived.swift
@@ -1,7 +1,13 @@
 //
-//  Exercise+Derived.swift
+//  ExerciseSet+Derived.swift
 //  DidWeights
 //
 //  Created by Abdulkarim Mziya on 2026-09-04.
 //
 
+
+extension ExerciseSet {
+    var isCompletable: Bool {
+        (reps ?? 0) > 0
+    }
+}
